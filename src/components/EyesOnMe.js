be3bd21f4154533e1b8700a1600dc819273f0f31
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-09-22 18:09:08 +0300

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+
+function EyesOnMe(){
+
+    function blur(){
+        console.log("Good!");
+    }
+
+    function focus(){
+        console.log("Hey! Eyes on me!");
+    }
+
+    return (
+        <div>
+            <button onFocus={blur} onBlur={focus}>Eyes on me</button>
+        </div>
+    )
+}
+export default EyesOnMe
